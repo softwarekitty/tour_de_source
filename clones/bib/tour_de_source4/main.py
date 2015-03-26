97f60f56474e86a4ebc7366c4c3798c79e127f9a
root <root@rat.(none)>
2015-03-25 21:40:53 -0500

    maybe create a virt env folder in the root of this dir for the clones to use
    Former-commit-id: 50204f12e45037a2dbe69ed18d4926ad3314d394

---
+++ clones/bib/tour_de_source4/main.py
@@ -0,0 +1,57 @@
+from os.path import expanduser
+HOME = expanduser("~")
+LOCAL_PATH = HOME + "/Documents/SoftwareProjects/tour_de_source/"
+import sys
+sys.path.append(LOCAL_PATH)
+
+import util
+from depot import Depot
+from tourist import Tourist
+# from tourist import LookiLoo
+from sourcer import GithubPythonSourcer
+# from sourcer import LocalTestSourcer
+from scanner import PythonRegexScanner
+
+
+cloneSuffix = "clones/bib/tour_de_source4/"
+credentials = "SienaGarner:mW3sTB6ZpFzVlCZzYMq"
+first = 393216
+stop = 524287
+endingMessage = "Tour of bib_4 ended with status: "
+
+
+BASE_PATH = LOCAL_PATH + cloneSuffix
+LOG_PATH = BASE_PATH + "data/log/"
+LOG_DEBUG_FILENAME = LOG_PATH + "tour_de_source.debug.log"
+LOG_CRITICAL_FILENAME = LOG_PATH + "tour_de_source.critical.log"
+
+
+# now make blank logging files
+open(LOG_DEBUG_FILENAME, 'a').close()
+open(LOG_CRITICAL_FILENAME, 'a').close()
+
+
+e1 = "investigationbot@gmail.com"
+p1 = "cro0thiezlutrl"
+
+to = "carlallenchapman@gmail.com"
+l = util.prepareLogging(e1, p1, to, BASE_PATH, LOG_DEBUG_FILENAME, LOG_CRITICAL_FILENAME)
+
+
+d = Depot(l, BASE_PATH)
+# test scanner or quickly send finished email
+# s = [LocalTestSourcer("copycat_test_sourcer", e1, p1, l, bp=BASE_PATH)]
+# t = Tourist(d, e1, p1, to, PythonRegexScanner(l), s, l, "LocalTestSourcer tour ended with status: ", LOG_CRITICAL_FILENAME)
+
+
+# test github interactions
+# s = [GithubPythonSourcer("mock", e1, p1, l)]
+# t = LookiLoo(d, e1, p1, to, PythonRegexScanner(l), s, l, "Lookilo finished with status: ", LOG_CRITICAL_FILENAME)
+
+# test normal operation or run normally
+# 15249309 will be a small, fast repo, 15249311 is two repos later
+s = [GithubPythonSourcer("20Commits", e1, p1, l, credentials, first, stop)]
+t = Tourist(d, e1, p1, to, PythonRegexScanner(l), s, l, endingMessage, LOG_CRITICAL_FILENAME)
+
+
+t.tour()
---
+++ clones/yen/tour_de_source8/main.py
@@ -0,0 +1,57 @@
+from os.path import expanduser
+HOME = expanduser("~")
+LOCAL_PATH = HOME + "/Documents/SoftwareProjects/tour_de_source/"
+import sys
+sys.path.append(LOCAL_PATH)
+
+import util
+from depot import Depot
+from tourist import Tourist
+# from tourist import LookiLoo
+from sourcer import GithubPythonSourcer
+# from sourcer import LocalTestSourcer
+from scanner import PythonRegexScanner
+
+
+cloneSuffix = "clones/yen/tour_de_source8/"
+credentials = "AxylSaunders:38geLsPoT8Cd0q4giOBRt"
+first = 4063232
+stop = 4194303
+endingMessage = "Tour of yen_8 ended with status: "
+
+
+BASE_PATH = LOCAL_PATH + cloneSuffix
+LOG_PATH = BASE_PATH + "data/log/"
+LOG_DEBUG_FILENAME = LOG_PATH + "tour_de_source.debug.log"
+LOG_CRITICAL_FILENAME = LOG_PATH + "tour_de_source.critical.log"
+
+
+# now make blank logging files
+open(LOG_DEBUG_FILENAME, 'a').close()
+open(LOG_CRITICAL_FILENAME, 'a').close()
+
+
+e1 = "investigationbot@gmail.com"
+p1 = "cro0thiezlutrl"
+
+to = "carlallenchapman@gmail.com"
+l = util.prepareLogging(e1, p1, to, BASE_PATH, LOG_DEBUG_FILENAME, LOG_CRITICAL_FILENAME)
+
+
+d = Depot(l, BASE_PATH)
+# test scanner or quickly send finished email
+# s = [LocalTestSourcer("copycat_test_sourcer", e1, p1, l, bp=BASE_PATH)]
+# t = Tourist(d, e1, p1, to, PythonRegexScanner(l), s, l, "LocalTestSourcer tour ended with status: ", LOG_CRITICAL_FILENAME)
+
+
+# test github interactions
+# s = [GithubPythonSourcer("mock", e1, p1, l)]
+# t = LookiLoo(d, e1, p1, to, PythonRegexScanner(l), s, l, "Lookilo finished with status: ", LOG_CRITICAL_FILENAME)
+
+# test normal operation or run normally
+# 15249309 will be a small, fast repo, 15249311 is two repos later
+s = [GithubPythonSourcer("20Commits", e1, p1, l, credentials, first, stop)]
+t = Tourist(d, e1, p1, to, PythonRegexScanner(l), s, l, endingMessage, LOG_CRITICAL_FILENAME)
+
+
+t.tour()
