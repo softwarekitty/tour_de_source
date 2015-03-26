a25828a89abc6ed564785498ca522cbf5812e167
Carl Chapman <carlallenchapman@gmail.com>
2015-03-25 19:34:56 -0500

    making cleaner.py much more powerfull...makes sure all the right directories exist and also cleans them out

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -1,8 +1,42 @@
 import glob
 import os
 import shutil
-from os.path import expanduser
+import commands
 
+
+# no exception if successfully able to erase whatever is at path
+def erasePath(path, logger=None):
+    if os.path.isfile(path):
+        try:
+            os.remove(path)
+        except Exception as e:
+            if logger:
+                logger.warning("failure to erase file: " + path + " exception: " + e.strerror)
+            raise e
+    else:
+        try:
+            cmd = "rm -rf " + path
+            result = commands.getstatusoutput(cmd)
+            if result is None:
+                raise RuntimeWarning("None result in util.erasePath")
+            if result[0] != 0:
+                raise RuntimeWarning("util.erasePath has non-zero result for trying to delete path: " + path)
+        except Exception as e:
+            if logger:
+                logger.warning("failure to erase non-file: " + path + " exception: " + e.strerror)
+            raise e
+
+
+def eraseAt(basePath, eraseAll=False):
+    contents = [os.path.join(basePath, f) for f in os.listdir(basePath)]
+    if contents:
+        for x in contents:
+            if os.path.isfile(x) or eraseAll:
+                erasePath(x)
+
+
+# get the correct local path
+from os.path import expanduser
 HOME = expanduser("~")
 LOCAL_PATH = HOME + "/Documents/SoftwareProjects/tour_de_source/"
 
@@ -10,20 +44,18 @@ cloneNames = ["bib", "gum", "rat", "yen"]
 
 for name in cloneNames:
     for i in range(1, 9):
+
         basePath = LOCAL_PATH + "clones/" + name + "/tour_de_source" + str(i) + "/"
-        dataPath = basePath + "data/"
-        # remove all python files?
-        print "basepath: " + basePath
-        for f in glob.glob(basePath + "*.pyc"):
-            print "removing: " + f
-            os.remove(f)
-        for f in glob.glob(dataPath + "*.db"):
-            os.remove(f)
-        for x in glob.glob(dataPath + "log/*"):
-            os.remove(x)
-        for y in glob.glob(basePath + "repo/*"):
-            if os.path.isfile(y):
-                os.remove(y)
-            else:
-                shutil.rmtree(y)
+        if not os.path.exists(basePath):
+            os.makedirs(basePath)
+        if not os.path.exists(basePath + "repo/"):
+            os.makedirs(basePath + "repo/")
+        if not os.path.exists(basePath + "data/"):
+            os.makedirs(basePath + "data/")
+        if not os.path.exists(basePath + "data/log/"):
+            os.makedirs(basePath + "data/log/")
+        eraseAt(basePath)
+        eraseAt(basePath + "data/")
+        eraseAt(basePath + "data/log/")
+        eraseAt(basePath + "repo/", True)
 print "done deleting all logs, repos and databases"
