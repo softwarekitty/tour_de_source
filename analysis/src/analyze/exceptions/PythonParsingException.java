2a1d281a2c1bb7ec4ea93eae9b4346b4f9ef03b0
Carl Chapman <carlallenchapman@gmail.com>
2015-04-24 14:21:10 -0500

    git problems

---
+++ analysis/src/analyze/exceptions/PythonParsingException.java
@@ -0,0 +1,14 @@
+package analyze.exceptions;
+
+public class PythonParsingException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5012907273773139019L;
+
+	public PythonParsingException(String s){
+		super(s);
+	}
+
+}
