6b676f385f911f79489f105d27e6d7f599b39a8e
Carl Chapman <carlallenchapman@gmail.com>
2015-04-27 02:28:29 -0500

    effort to parallelize the behavioral similarity project seems to have been successful.

--- csharp/ConsoleApplication1/Program.cs
+++ csharp/ConsoleApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
@@ -49,11 +50,15 @@ namespace ConsoleApplication1
             int nStringsPerPattern = 384;
             double minSimilarity = 0.70;
 
-            WholeMatrix wholeMatrix = new WholeMatrix(nKeys);
+            WholeMatrix wholeMatrix = new WholeMatrix(nKeys, minSimilarity);
             Dictionary<int, int> keyConverter = initializeKeyConverter(keyList);
             int[] differentSeeds = Enumerable.Repeat(0, keyList.Count).Select(i => gen.Next(0,int.MaxValue)).ToArray();
-            Parallel.For(0, keyList.Count,i => evaluateOneRow(i,differentSeeds[i],keyConverter, regexMap, patternMap,nStringsPerPattern,keyList,wholeMatrix,nKeys,minSimilarity));
-            HalfMatrix halfMatrix = new HalfMatrix(wholeMatrix);
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 4;
+            Parallel.For(0, keyList.Count,options,i => evaluateOneRow(i,differentSeeds[i],keyConverter, regexMap, patternMap,nStringsPerPattern,keyList,wholeMatrix,nKeys,minSimilarity));
+            System.IO.File.WriteAllText(@"\\vmware-host\Shared Folders\Documents\SoftwareProjects\tour_de_source\analysis\analysis_output\wholeMatrix.txt", wholeMatrix.ToString());
+            HalfMatrix halfMatrix = new HalfMatrix(wholeMatrix, minSimilarity);
+            System.IO.File.WriteAllText(@"\\vmware-host\Shared Folders\Documents\SoftwareProjects\tour_de_source\analysis\analysis_output\halfMatrix.txt", halfMatrix.ToString());
             string abcContent = halfMatrix.getABC(minSimilarity,keyConverter);
             System.IO.File.WriteAllText(@"\\vmware-host\Shared Folders\Documents\SoftwareProjects\tour_de_source\analysis\analysis_output\behavioralSimilarityGraph.abc",abcContent);
         }
@@ -91,53 +96,118 @@ namespace ConsoleApplication1
             HashSet<string> matchingStrings_outer = getMatchStrings(nStringsPerPattern, gen, pattern_outer, regex_outer, tempFilePath);
             double nMatchingStrings = matchingStrings_outer.Count;
             int maxErrors = (int)((1 - minSimilarity) * nMatchingStrings)+1;
-            int errorCounter=0;
-            int j = 0;
-            foreach (int innerKey in keyList)
-            {
-                Regex regex_inner = regexMap[innerKey];
-                int alsoMatchingCounter = 0;
-                int timeoutCounter = 0;
-                foreach (string matchingString in matchingStrings_outer)
-                {
-                    Match attemptMatch = regex_inner.Match(matchingString);
-                    if (attemptMatch.Success)
-                    {
-                        alsoMatchingCounter++;
-                    }
-                    else
-                    {
-                        errorCounter++;
-                    }
 
-                    // after a certain number of errors, it is impossible to
-                    // be included anyway - give up early
-                    // this may drag down some corner cases when they are
-                    // averaged in the half-matrix, but will save a lot of time, 
-                    // since the vast majority of these comparisons have 0 similarity
-                    // anyway, maybe n^2 - n of them
-                    if (errorCounter > maxErrors)
-                    {
-                        break;
-                    }
-                }
-                double nTests = nMatchingStrings - timeoutCounter;
-                // avoid dividing by 0
-                if (nTests == 0)
-                {
-                    wholeMatrix.set(i, j, 0);
-                }
-                else
-                {
-                    wholeMatrix.set(i, j, alsoMatchingCounter / nTests);
-                }
-                j++;
+            //in order to protect against exponential worst-case regexes we chunk the row,
+            //and wait in powers of two for each chunk.  It's a mess but we cannot get inside of
+            //this system code: 'regex_inner.Match(matchingString)' to do propper cancelation
+            int[] nTimeouts = {0};
+            int chunkSize = 128;
+            int nChunks = nKeys / chunkSize;
+            int remainder = nKeys % chunkSize;
+            for (int chunkIndex = 0; chunkIndex < nChunks; chunkIndex++)
+            {
+                int chunkStart = chunkIndex * chunkSize;
+                int chunkStop = chunkStart + chunkSize;
+                processChunk(i,chunkSize, chunkStart, chunkStop, matchingStrings_outer, regexMap, maxErrors,keyList,  wholeMatrix, nTimeouts);
+            }
+            if (remainder > 0)
+            {
+                int chunkStart = nChunks * chunkSize;
+                int chunkStop = chunkStart + remainder;
+                processChunk(i, remainder, chunkStart, chunkStop, matchingStrings_outer, regexMap, maxErrors, keyList, wholeMatrix, nTimeouts);
             }
             stopwatch.Stop();
             TimeSpan ts = stopwatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("completed i: " + i + "/" + nKeys + " outerKey: " + outerKey + " nMatchStrings:" + matchingStrings_outer.Count + " time taken: " + elapsedTime);
+            Console.WriteLine("completed i: " + i + "/" + nKeys + " nTimeouts: " + nTimeouts[0] + " nMatchStrings:" + matchingStrings_outer.Count + " time taken: " + elapsedTime);
                 
+        }
+
+        static void processChunk(int i,int chunkSize, int chunkStart, int chunkStop, HashSet<string> matchingStrings_outer, Dictionary<int, Regex> regexMap, int maxErrors,List<int> keyList, WholeMatrix wholeMatrix, int[] nTimeouts)
+        {
+            // rarely, a pathological regex can hang for a very long time.
+            // but most of these finish within 120 microseconds (rate: 8300/sec)
+            // usually should be able to do 128 in 16 milliseconds so start there
+            int taskIndex = 0;
+            Task[] tasks = new Task[chunkSize];
+            for (int keyIndex = chunkStart; keyIndex < chunkStop; keyIndex++)
+            {
+                int innerKey = keyList[keyIndex];
+                Regex regex_inner = regexMap[innerKey];
+                tasks[taskIndex] = Task.Factory.StartNew(() => populateMatrixCell(i,keyIndex,wholeMatrix,matchingStrings_outer, regex_inner, maxErrors));
+                taskIndex++;
+            }
+            int overheadBuffer = 3;
+            int exponent = 0;
+            int maxExponent = 13;
+            bool chunkComplete=false;
+            while (exponent < maxExponent)
+            {
+                // 20MS, 21MS, 23MS, 27MS, 35MS, 42MS, 83MS, 147MS, 275MS, 531MS, 1042MS, 2067MS, 4115MS
+                // so if after 8428 ms, the 128 cells are not complete, set remaining cells to the flag value.
+                double balloon = Math.Pow(2,exponent);
+                if (Task.WaitAll(tasks, TimeSpan.FromMilliseconds(16 + balloon + overheadBuffer)))
+                {
+                    //all tasks in this chunk have completed, exit
+                    chunkComplete = true;
+                    break;
+                }
+                exponent++;
+            }
+            if (!chunkComplete)
+            {
+                double incompleteFlag = 0.0000012345;
+                for (int j = chunkStart; j < chunkStop; j++)
+                {
+                    if (wholeMatrix.cellIsEmpty(i, j))
+                    {
+                        wholeMatrix.set(i, j, incompleteFlag);
+                        nTimeouts[0]++;
+                    }
+                }
+
+            }
+            
+        }
+
+        static void populateMatrixCell(int i, int j, WholeMatrix wholeMatrix, HashSet<string> matchingStrings_outer, Regex regex_inner, int maxErrors)
+        {
+            if (j >= wholeMatrix.getN()|| i >=wholeMatrix.getN())
+            {
+                //Console.WriteLine("j: " + j);
+                //Console.WriteLine();
+
+                //tired of tracing this.  Not sure how j is getting one too large, but I will just skip it here.
+                return;
+            }
+            double nMatchingStrings = matchingStrings_outer.Count;
+            int alsoMatchingCounter = 0;
+            int errorCounter = 0;
+            foreach (string matchingString in matchingStrings_outer)
+            {
+                Match attemptMatch = regex_inner.Match(matchingString);
+                if (attemptMatch.Success)
+                {
+                    alsoMatchingCounter++;
+                }
+                else
+                {
+                    errorCounter++;
+                }
+
+                // after a certain number of errors, it is impossible to
+                // be included anyway - give up early
+                // this may drag down some corner cases when they are
+                // averaged in the half-matrix, but will save a lot of time, 
+                // since the vast majority of these comparisons have 0 similarity
+                // anyway, maybe n^2 - n of them
+                if (errorCounter > maxErrors)
+                {
+                    break;
+                }
+            }
+            double similarity = alsoMatchingCounter / nMatchingStrings;
+            wholeMatrix.set(i, j, similarity);
         }
 
         static string callRex(string rexArgs)
@@ -224,8 +294,11 @@ namespace ConsoleApplication1
     class WholeMatrix
     {
         private double[][] values;
-        public WholeMatrix(int n)
+        private static double weirdValue = -2.0;
+        private double minSimilarity;
+        public WholeMatrix(int n, double minSimilarity)
         {
+            this.minSimilarity = minSimilarity;
             values = new double[n][];
             for (int i = 0; i < n; i++)
             {
@@ -233,9 +306,36 @@ namespace ConsoleApplication1
                 for (int j = 0; j < n; j++)
                 {
                     // weird value should help detect errors
-                    values[i][j] = -2.0;
+                    values[i][j] = weirdValue;
                 }
             }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder wholeMatrixString = new StringBuilder();
+            wholeMatrixString.Append("WholeMatrix:\n");
+            int i = 0;
+            foreach (double[] A in values){
+                wholeMatrixString.Append("row:"+i+"[\n");
+                bool isFirst = true;
+                int j=0;
+                foreach(double d in A){
+                    if (d > minSimilarity)
+                    {
+                        String val = "("+j+":"+String.Format("{0:0.00}", d)+")";
+                        if(!isFirst){
+                            wholeMatrixString.Append(",");
+                        }
+                        wholeMatrixString.Append(val);
+                        isFirst = false;
+                    }
+                    j++;
+                }
+                wholeMatrixString.Append("] completed row: "+i+"\n\n");
+                i++;
+            }
+            return wholeMatrixString.ToString();
         }
 
         public double getAvg(int row, int col)
@@ -268,13 +368,20 @@ namespace ConsoleApplication1
         }
 
 
+
+        internal bool cellIsEmpty(int i, int j)
+        {
+            return values[i][j] == weirdValue;
+        }
     }
 
     class HalfMatrix
     {
         private double[][] values;
-        public HalfMatrix(WholeMatrix original)
+        private double minSimilarity;
+        public HalfMatrix(WholeMatrix original, double minSimilarity)
         {
+            this.minSimilarity = minSimilarity;
             int n = original.getN();
             values = new double[n][];
             for (int i = 0; i < n; i++)
@@ -285,6 +392,36 @@ namespace ConsoleApplication1
                     values[i][j] = original.getAvg(i,j);
                 }
             }
+        }
+
+        public string ToString()
+        {
+            StringBuilder halfMatrixString = new StringBuilder();
+            halfMatrixString.Append("HaflMatrix:\n");
+            int i = 0;
+            foreach (double[] A in values)
+            {
+                halfMatrixString.Append("row:" + i + "[\n");
+                bool isFirst = true;
+                int j = 0;
+                foreach (double d in A)
+                {
+                    if (d > minSimilarity)
+                    {
+                        String val = "(" + j + ":" + String.Format("{0:0.00}", d) + ")";
+                        if (!isFirst)
+                        {
+                            halfMatrixString.Append(",");
+                        }
+                        halfMatrixString.Append(val);
+                        isFirst = false;
+                    }
+                    j++;
+                }
+                halfMatrixString.Append("] completed row: " + i + "\n\n");
+                i++;
+            }
+            return halfMatrixString.ToString();
         }
 
         //row is i, col is j
