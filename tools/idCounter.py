5b7c87ca01500ca11aaae8f4808c3526171bc7e5
Carl Chapman <carlallenchapman@gmail.com>
2016-01-29 04:45:17 -0600

    added some ideas about how to draw out some more interesting conclusions from the study

---
+++ tools/idCounter.py
@@ -0,0 +1,10 @@
+x = set()
+sourcePath = "/Users/carlchapman/Documents/SoftwareProjects/tour_de_source/analysis/behavioral_clustering/behavioralSimilarityGraph.abc"
+with open (sourcePath, "r") as edges:
+    content = edges.readlines()
+    for edge in content:
+        els = edge.split(" ")
+        x.add(els[0])
+        x.add(els[1])
+print(len(x))
+        
